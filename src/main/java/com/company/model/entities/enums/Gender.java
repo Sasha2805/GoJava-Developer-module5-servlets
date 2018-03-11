74535806997fdedec037301287cc107a32494f99
Sasha Kaminska <sasha249133@ukr.net>
2018-03-11 18:53:53 +0200

    add enums

---
+++ src/main/java/com/company/model/entities/enums/Gender.java
@@ -0,0 +1,5 @@
+package com.company.model.entities.enums;
+
+public enum Gender {
+    MALE, FEMALE
+}
---
+++ src/main/java/com/company/model/entities/enums/SkillLevel.java
@@ -0,0 +1,5 @@
+package com.company.model.entities.enums;
+
+public enum SkillLevel {
+    JUNIOR, MIDDLE, SENIOR
+}
